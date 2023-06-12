511f227875071a2983adb2a9e5c5f22425b6d7de
haruhash <130542725+haruhash@users.noreply.github.com>
2023-06-12 19:04:19 +0700

    change app, requirements files

--- app.py
+++ app.py
@@ -1,49 +1,89 @@
 import numpy as np
-from keras.preprocessing.image import load_img, img_to_array
-from keras.models import load_model
-from flask import Flask, jsonify, request
+import streamlit as st
+import time
+from PIL import Image
+from keras.preprocessing import image
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+from tensorflow.keras.losses import sparse_categorical_crossentropy
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-model = load_model('FV.h5')
+train = 'train'
+# Create a generator
+train_datagen = ImageDataGenerator(
+  rescale=1./255
+)
+train_datagen = train_datagen.flow_from_directory(
+        train,
+        batch_size=32,
+        target_size=(300, 300),
+        class_mode='sparse')
 
-labels = {0: 'apple', 1: 'banana', 2: 'beetroot', 3: 'bell pepper', 4: 'cabbage', 5: 'capsicum', 6: 'carrot',
-          7: 'cauliflower', 8: 'chilli pepper', 9: 'corn', 10: 'cucumber', 11: 'eggplant', 12: 'garlic', 13: 'ginger',
-          14: 'grapes', 15: 'jalepeno', 16: 'kiwi', 17: 'lemon', 18: 'lettuce',
-          19: 'mango', 20: 'onion', 21: 'orange', 22: 'paprika', 23: 'pear', 24: 'peas', 25: 'pineapple',
-          26: 'pomegranate', 27: 'potato', 28: 'raddish', 29: 'soy beans', 30: 'spinach', 31: 'sweetcorn',
-          32: 'sweetpotato', 33: 'tomato', 34: 'turnip', 35: 'watermelon'}
+labels = train_datagen.class_indices
+Labels = '\n'.join(sorted(train_datagen.class_indices.keys()))
+with open('Labels.txt', 'w') as file:
+  file.write(Labels)
+class_names = list(labels.keys())
 
+model=Sequential()
+#Convolution blocks
+model.add(Conv2D(32, kernel_size = (3,3), padding='same',input_shape=(300,300,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=2)) 
+model.add(Conv2D(64, kernel_size = (3,3), padding='same',activation='relu'))
+model.add(MaxPooling2D(pool_size=2)) 
+model.add(Conv2D(32, kernel_size = (3,3), padding='same',activation='relu'))
+model.add(MaxPooling2D(pool_size=2)) 
+#Classification layers
+model.add(Flatten())
+model.add(Dense(64,activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(32,activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(6,activation='softmax'))
+model.compile(optimizer = 'Adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
 
-def prepare_image(img_path):
-    img = load_img(img_path, target_size=(224, 224, 3))
-    img = img_to_array(img)
-    img = img / 255
-    img = np.expand_dims(img, [0])
-    answer = model.predict(img)
-    y_class = answer.argmax(axis=-1)
-    print(y_class)
-    y = " ".join(str(x) for x in y_class)
-    y = int(y)
-    res = labels[y]
-    print(res)
-    return res.capitalize()
-
-
-app = Flask(__name__)
-
-
-@app.route('/predict', methods=['POST'])
-def infer_image():
-    if 'file' not in request.files:
-        return jsonify(error="Please try again. The Image doesn't exist")
-
-    file = request.files.get('file')
-    img_bytes = file.read()
-    img_path = "./upload_images/test.jpg"
-    with open(img_path, "wb") as img:
-        img.write(img_bytes)
-    result = prepare_image(img_path)
-    return jsonify(prediction=result)
+def training():
+    model.fit_generator(train_datagen,
+            epochs=1,
+            steps_per_epoch=2753//32)
 
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+html_temp_2 = '''
+    <div style = "padding-bottom: 20px; padding-top: 20px; padding-left: 20px; padding-right: 20px">      
+    <center><h2>Fruit Classifier</h2></center>
+    </div>
+    '''
+st.markdown(html_temp_2, unsafe_allow_html=True)
+st.set_option('deprecation.showfileUploaderEncoding', False)
+select = st.selectbox("Please select how you want to upload the image",("Please Select","Upload image via link","Upload image from device"))
+if select == "Upload image via link":
+    try:
+        img = st.text_input('Enter the Image Address')
+        img = Image.open(urllib.request.urlopen(img))
+    except:
+        if st.button('Submit'):
+            show = st.error("Please Enter a valid Image Address!")
+            time.sleep(4)
+            show.empty()
+
+if select == 'Upload image from device':
+    file = st.file_uploader('Select', type = ['jpg', 'png', 'jpeg'])
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+    if file is not None:
+        img = Image.open(file)
+
+try:
+    if img is not None:
+        st.image(img, width = 300, caption = 'Uploaded Image')
+        if st.button('Predict'):
+            img = np.array(img.resize((300, 300), Image.ANTIALIAS))
+            img = np.array(img, dtype='uint8')
+            img = np.array(img)/255.0
+            training()
+            prediction = model.predict(img[np.newaxis, ...])
+            predicted_class = class_names[np.argmax(prediction[0], axis=-1)]
+            st.success("Classified Class is : {}".format(predicted_class))
+
+except:
+    pass
