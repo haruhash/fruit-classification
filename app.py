0a6cfd4cc73f9047be3e4abbe349549b9daf1c86
haruhash <130542725+haruhash@users.noreply.github.com>
2023-06-13 22:11:22 +0700

    App Update

--- app.py
+++ app.py
@@ -5,13 +5,28 @@ from fastai.vision.all import (
 import urllib.request
 import glob
 import streamlit as st
+import requests
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
     
+imdb_url = 'https://www.kasetprice.com/ราคา/มะม่วง/วันนี้'
+imdb_response = requests.get(imdb_url)
+imdb_soup = BeautifulSoup(imdb_response.text, 'html.parser')
+group = imdb_soup.find_all('div', {'class': 'price-list-cost'})
+
 MODEL_URL = "https://huggingface.co/spaces/haruhash/fruit-classification/resolve/main/fruit.pkl"
 urllib.request.urlretrieve(MODEL_URL, "fruit.pkl")
 learn_inf = load_learner('fruit.pkl')
 
+if "button_clicked" not in st.session_state:
+    st.session_state.button_clicked = False
+
+def callback():
+    st.session_state.button_clicked = True
+
 def get_image_from_upload():
-    uploaded_file = st.file_uploader("Upload Fruit Pictures Files",type=['png','jpeg', 'jpg'])
+    uploaded_file = st.file_uploader("Upload Picture",type=['png','jpeg', 'jpg'])
     if uploaded_file is not None:
         st.image(PILImage.create((uploaded_file)))
         return PILImage.create((uploaded_file))
@@ -28,12 +43,16 @@ def predictMango(learn, img):
     if pred == '00':
         mnType = "Extra"
     elif pred == '01':
-        mnType = "Class 1"
+        mnType = "Grade A"
     elif pred == '02':
-        mnType = "Class 2"
+        mnType = "Grade B"
     else:
-        mnType = "Class 3"
-    st.success(f"This is {mnType} mango with the probability of {pred_prob[pred_idx]*100:.02f}%")
+        mnType = "r"
+    if mnType == "r":
+        st.error(f"This is a rotten mango with the probability of {pred_prob[pred_idx]*100:.02f}%")
+    else:
+        st.success(f"This is {mnType} mango with the probability of {pred_prob[pred_idx]*100:.02f}%")
+    return int(pred)
 
 def predict(learn, img):
     pred, pred_idx, pred_prob = learn.predict(img)
@@ -46,7 +65,7 @@ def predict(learn, img):
         MODEL_URL = "https://huggingface.co/spaces/haruhash/fruit-classification/resolve/main/mangograde.pkl"
         urllib.request.urlretrieve(MODEL_URL, "mangograde.pkl")
         learn_inf = load_learner('mangograde.pkl')
-        predictMango(learn_inf, img)
+        tp = predictMango(learn_inf, img)
     elif pred=='03':
         frType = "Orange"
     elif pred=='04':
@@ -55,15 +74,31 @@ def predict(learn, img):
         st.error(f"Others with the probability of {pred_prob[pred_idx]*100:.02f}%")
         return None
     st.success(f"This is {frType} with the probability of {pred_prob[pred_idx]*100:.02f}%")
+    if pred == '02' and tp != 3:
+        mList = ["เขียวเสวย", "แก้ว", "โชคอนันต์(สุก)", "โชคอนันต์(ดิบ)", "น้ำดอกไม้(แก่)", "น้ำดอกไม้(สุก)", "น้ำดอกไม้(อ่อน)", "ฟ้าลั่น", "มันเดือนเก้า", "อกร่อง(ดิบ)", "อกร่อง(สุก)", "อื่นๆ"]
+        seldat = st.selectbox('Type of mango', mList)
+        confirm = st.button('Confirm')
+        if confirm:
+            i = 0
+            for mStr in mList:
+                if seldat == mStr:
+                    nStr = group[i].string.strip()
+                    if nStr == '-':
+                        st.error(f"No price data")
+                    else:
+                        st.success(f"Price : {((4 - tp) / 2 * np.float_(nStr)):.02f}/kg")
+                    break
+                i = i + 1
+
 def main():
-    st.title('Fruit Classification Model')
-    datasrc = st.sidebar.radio("Select input source.", ["Uploaded file", "Take a picture"])
-    if datasrc == "Uploaded file": 
+    st.title('Mango Classification Model')
+    datasrc = st.sidebar.selectbox("Type of picture input", ["Upload", "Camera"])
+    if datasrc == "Upload": 
         image = get_image_from_upload()
     else:
         image = take_a_picture()
-    result = st.button('Classify')
-    if result:
+    result = st.button(label = 'Classify', on_click = callback)
+    if result or st.session_state.button_clicked:
         predict(learn_inf, image)        
     
 if __name__ == '__main__':
